3f5dea9df9653b23ea57365c61a56075ab5f282d
TranDucThinhK17HL <thinhtdhe173229@fpt.edu.vn>
2024-09-16 18:01:36 +0700

    impl header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,15 +1,27 @@
 import React from 'react';
-import { Navbar, Container } from 'react-bootstrap';
 
-const Header: React.FC = () => {
+export default function Header() {
+    const headerStyle: React.CSSProperties = {
+        backgroundColor: "#BC9F8B",
+    }
     return (
-        <Navbar bg="primary" variant="dark" expand="lg">
-            <Container>
-                <Navbar.Brand href="/">CLMS</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            </Container>
-        </Navbar>
+        <header className="flex-row header text-white p-3" style={headerStyle}>
+            <nav className="navbar navbar-expand-lg navbar-dark container-fluid p-3">
+                <a className="navbar-brand" href="/">
+                    <h2>CLMSby3LUA</h2>
+                </a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav ms-auto">
+                        <li className="nav-item">
+                            <a className="nav-link" href="/login"><h5>Đăng Nhập</h5></a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
     );
-};
-
-export default Header;
+}
